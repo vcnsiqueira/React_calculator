2b2fb95b8aac0ec5fec486c9096d082c9648b154
vcnsiqueira <35041704+vcnsiqueira@users.noreply.github.com>
2020-05-27 23:30:49 -0300

    Including the state to check if the last click was a number (lastClickIsNumber). Isolating the function that perform arithmetical operations and creating a new function to deal with the equal button press event

--- src/App.js
+++ src/App.js
@@ -10,14 +10,16 @@ class App extends Component {
       currentFactor: 0,
       firstFactor: '',
       firstOperation: '',
-      currentOperation: ''
+      currentOperation: '',
+      lastClickIsNumber: ''
     };
 
   }
 
   handleCurrentFactor = event => {
     this.setState({
-      currentFactor: event.target.value
+      currentFactor: event.target.value,
+      lastClickIsNumber: true
     });
   };
 
@@ -29,13 +31,21 @@ class App extends Component {
       this.setState({
         firstOperation: event.target.id,
         currentOperation: event.target.id,
-        firstFactor: this.state.currentFactor
+        firstFactor: this.state.currentFactor,
+        lastClickIsNumber: false
+      })
+    } else if(!this.state.lastClickIsNumber){
+      this.setState({
+        firstOperation: event.target.id,
+        currentOperation: event.target.id
       })
     } else {
-      //this.evaluateOperation();
+      const result = this.evaluateOperation(); // alterar o evaluateOperation para apenas retornar o resultado e não atualizar os states
       this.setState({
         currentOperation: event.target.id,
-        firstFactor: this.state.currentFactor
+        firstFactor: result,
+        currentFactor: result,
+        lastClickIsNumber: false
       })
     }
     console.log(`Primeiro fator: ${ultimo}`);
@@ -63,11 +73,20 @@ class App extends Component {
         alert("Houve algum problema na seleção da operação!")
     }
     console.log(result)
+    return result;
+  }
+
+  handleEqualButton = () => {
+    if(this.state.currentOperation === '' || !this.state.lastClickIsNumber) {
+      return null;
+    }
+    const result = this.evaluateOperation()
     this.setState({
       firstFactor: '',
       currentFactor: result,
       firstOperation: '',
-      currentOperation: ''
+      currentOperation: '',
+      lastClickIsNumber: false
     });
   };
 
@@ -80,11 +99,12 @@ class App extends Component {
           <button id="-" onClick={this.handleOperationButton}>Subtrair</button>
           <button id="*" onClick={this.handleOperationButton}>Multiplicar</button>
           <button id="/" onClick={this.handleOperationButton}>Dividir</button>
-          <button id="=" onClick={this.evaluateOperation}>Igual</button>
+          <button id="=" onClick={this.handleEqualButton}>Igual</button>
           <h5>{`Primeiro fator: ${this.state.firstFactor}`}</h5>
-          <h5>{`Segundo fator: ${this.state.currentFactor}`}</h5>
+          <h5>{`Fator atual: ${this.state.currentFactor}`}</h5>
           <h5>{`Primeira operação: ${this.state.firstOperation}`}</h5>
           <h5>{`Operação atual: ${this.state.currentOperation}`}</h5>
+          <h5>{`Último clique: ${this.state.lastClickIsNumber}`}</h5>
       </div>
     );
   }
